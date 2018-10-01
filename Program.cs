9b978af1a027985c606211faaaa468dd9839ae8e
Kawaii-Tsu <32310075+Kawaii-Tsu@users.noreply.github.com>
2018-10-01 14:26:40 -0600

    luckysevens demo

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace luckysevens
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalNuberofSevens = 0;
+            Random rng = new Random();
+
+            int die1;
+            int die2;
+
+            for (int i = 1; i <= 100; i++)
+            {
+                die1 = rng.Next(1,7);
+                die2 = rng.Next(1,7);
+
+                if (die1 + die2 == 7)
+                totalNuberofSevens++;
+            }
+
+            Console.WriteLine("Out of 100 rolls, you rolled {0} sevens", totalNuberofSevens);
+            Console.ReadLine();
+        }
+    }
+}
